31ab5bdf7847eacf11aee2e3ebbea042f7782b93
pjmk007 <60164230+pjmk007@users.noreply.github.com>
2021-03-20 22:56:29 +0900

    Create 1.py

---
+++ crawling/1.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('https://codingeverybody.github.io/scraping_sample/1.html')
+# print(r.text)
+soup = BeautifulSoup(r.text, 'html.parser')
+print('Title : ' + soup.title.string)
+articles = soup.findAll('div', {'class' : 'em'})
+print('Article : ' + articles[0].text)
